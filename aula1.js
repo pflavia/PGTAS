8230dd568cf7f610308d98ccbaa0985e21782c68
Flávia Pinheiro <pflavia@users.noreply.github.com>
2024-04-05 22:35:03 -0300

    Add files via upload

---
+++ aula1.js
@@ -0,0 +1,35 @@
+console.log("Happy Tests\n")
+
+// Declarando a variável para armazenar o nome
+let nome;
+
+// Atribuo o texto Julio a variável nome
+nome = "Marcos";
+
+/*
+   Comento aqui
+   Comento aqui
+   Comento aqui
+   Comento aqui
+   Comento aqui
+*/
+
+// Apresento o conteúdo da variável no Console
+console.log(nome)
+console.log(`Meu nome é ${nome} e a Sandra é a minha esposa, a CEO da casa`);
+
+console.log("\nTeste Flávio do Git\n\n")
+let name;
+name = "Flávio"
+console.log("Happy testing, " + name)
+
+/* 
+    Block comment here!
+    */
+
+    //Normal comment here...
+
+let love = "Arabella"
+
+console.log(name + " loves " + love)
+console.log (`O ${name} faz tudo que a ${love} mandar`)
